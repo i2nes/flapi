bba65261d582ea7129e10e968b8c0d5e07687a1e
Joao Antunes <jmfantunes@gmail.com>
2018-11-17 18:10:26 +0000

    implemented users create api

--- src/api-service/blueprints/api/routes.py
+++ src/api-service/blueprints/api/routes.py
@@ -1,20 +1,56 @@
 import logging
 from . import api
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from werkzeug.http import HTTP_STATUS_CODES
+from werkzeug.exceptions import BadRequest, BadGateway
+from ..helpers import meta, isvalidEmail
+from ..models import User
 
 
-@api.route('hello', methods=['GET'])
-def hello_api():
+"""
+curl -X POST http://localhost:8080/user/create \
+-H 'content-type: application/json' \
+-d '{
+  "email": "john.doe@gmail.com",
+  "first_name": "John",
+  "last_name": "Doe"
+}'
+"""
+@api.route('user/create', methods=['POST'])
+def user_create_api():
 
-    hello = {
-        'content': {
-            'text': 'hello world',
-        },
-        'meta': {
-            'httpCode': 200,
-            'httpText': HTTP_STATUS_CODES.get(200),
-        }        
-    }
+    try:
+        request_body = request.get_json()
 
-    return jsonify(hello), 200
+    except BadRequest as e:
+        return jsonify({'meta': meta(e.code, e.description)}), e.code
+
+    except Exception as e:
+        logging.info('Untreated exception: {}'.format(type(e)))
+        logging.info('Status code: {}, Message: {}'.format(e.code, e.description))
+        return jsonify({'meta': meta(e.code, e.description)}), e.code
+
+    else:
+        if not 'email' in request_body.keys() or not isvalidEmail(request_body["email"]):
+            return jsonify({'meta': meta(400, "Missing or unexpected email value")}), 400
+
+        if not 'first_name' in request_body.keys() or not 'last_name' in request_body.keys():
+            return jsonify({'meta': meta(400, "Missing or unexpected first_name or last_name value")}), 400
+
+        new_user = User.get_by_id(str(request_body['email'].lower()))
+
+        if new_user is not None:
+            return jsonify({'meta': meta(409, "User already exists")}), 409
+
+        new_user = User(id=str(request_body["email"]).lower())
+        new_user.first_name = str(request_body["first_name"]).capitalize()
+        new_user.last_name = str(request_body["last_name"]).capitalize()
+
+        email = new_user.put()
+        
+        response = {
+            'content': new_user.content(),
+            'meta': meta(200, HTTP_STATUS_CODES.get(200)),
+        }
+
+        return jsonify(response), 200
--- src/api-service/blueprints/models.py
+++ src/api-service/blueprints/models.py
@@ -1,1 +1,49 @@
-# Datastore Models
+from google.appengine.ext import ndb
+from werkzeug.security import generate_password_hash, check_password_hash
+from config import hostname
+
+
+class User(ndb.Model):
+
+    # User(id=email)
+    first_name = ndb.StringProperty()
+    last_name = ndb.StringProperty()
+    password = ndb.StringProperty()
+    isAdmin = ndb.BooleanProperty(default=False)
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    updated = ndb.DateTimeProperty(auto_now=True)
+
+    def content(self):
+
+        content = {
+            'email': self.key.id(),
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'isAdmin': self.isAdmin,
+            'created': self.created,
+            'updated': self.updated,
+            'links': [
+                {
+                    'rel': 'self',
+                    'method': 'get',
+                    'url': '{}/user/{}'.format(hostname, self.key.id()),
+                },
+                {
+                    'rel': 'delete',
+                    'method': 'delete',
+                    'url': '{}/user/{}'.format(hostname, self.key.id()),
+                },
+                {
+                    'rel': 'update',
+                    'method': 'post',
+                    'url': '{}/user/{}'.format(hostname, self.key.id()),
+                    'parameters': {
+                        'first_name': 'string',
+                        'last_name': 'string',
+                    },
+                },
+            ],
+        }
+
+        return content
+
